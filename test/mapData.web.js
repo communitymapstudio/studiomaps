d17c6ff1306e02a4fd10c0ce7096a86e55d84989
Jing Zhang <135781578+communitymapstudio@users.noreply.github.com>
2025-08-06 17:59:08 -0400

    Create mapData.web.js

---
+++ test/mapData.web.js
@@ -0,0 +1,13 @@
+import { Permissions, webMethod } from "wix-web-module";
+import wixData from "wix-data";
+
+export const getMarkers = webMethod(Permissions.Anyone, async () => {
+    const result = await wixData.query("Markers").limit(1000).find();
+
+    return result.items.map(item => ({
+        title: item.title,
+        description: item.description,
+        lat: item.lat,
+        lng: item.lng
+    }));
+});
